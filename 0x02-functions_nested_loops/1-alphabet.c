5d03495f042ecf1f6423af95595bee13a95901a9
ANAS AHMED <86914300+baghdadsniper@users.noreply.github.com>
2023-03-22 01:10:18 +0200

    wcwd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char c, i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (c = 'a'; c <= 'z'; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
+
+		_putchar(c);
 		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
